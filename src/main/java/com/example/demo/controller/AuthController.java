90112fa41f6805b56754ff26bf4ffb25acfdc1ab
bala-amypo <bala@amypo.in>
2025-12-20 15:17:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -511,27 +511,12 @@ public interface UserAccountRepository
 
 Exception:=====
 
-package com.example.demo.exception;
 
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
 
 
 ===
 
 
-package com.example.demo.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
 
 
 ====
