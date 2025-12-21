9e721802e3852fccf5b1cf827ab0e7f4af0df2d6
bala-amypo <bala@amypo.in>
2025-12-21 09:32:03 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -20,22 +20,4 @@ public class OpenApiConfig {
 }
 
 
-// package com.example.demo.config;
 
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.info.Info;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// @Configuration
-// public class OpenApiConfig {
-
-//     @Bean
-//     public OpenAPI openAPI() {
-//         return new OpenAPI()
-//                 .info(new Info()
-//                         .title("Employee Equipment Issuance API")
-//                         .version("1.0")
-//                         .description("Swagger UI for Employee Equipment Issuance Policy Checker"));
-//     }
-// }
