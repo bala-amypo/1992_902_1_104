4d5f5aa4ec6867752f8f5a56ca377d6276ab2195
bala-amypo <bala@amypo.in>
2025-12-21 09:26:02 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,41 +1,41 @@
-// package com.example.demo.config;
-
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.servers.Server;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import java.util.List;
-
-// @Configuration
-// public class OpenApiConfig {
-
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI()
-//                 // You need to change the port as per your server
-//                 .servers(List.of(
-//                         new Server().url("https://9005.vs.amypo.ai")
-//                 ));
-//         }
-// }
-
-
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Employee Equipment Issuance API")
-                        .version("1.0")
-                        .description("Swagger UI for Employee Equipment Issuance Policy Checker"));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9515.pro604cr.amypo.ai/")
+                ));
+        }
 }
+
+
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Info;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// public class OpenApiConfig {
+
+//     @Bean
+//     public OpenAPI openAPI() {
+//         return new OpenAPI()
+//                 .info(new Info()
+//                         .title("Employee Equipment Issuance API")
+//                         .version("1.0")
+//                         .description("Swagger UI for Employee Equipment Issuance Policy Checker"));
+//     }
+// }
