4878fa5c737e5ff9591129ee327026bfdb8155b5
bala-amypo <bala@amypo.in>
2025-12-25 12:36:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/PolicyRuleRepository.java
+++ src/main/java/com/example/demo/repository/PolicyRuleRepository.java
@@ -2,15 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.PolicyRule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface PolicyRuleRepository
-        extends JpaRepository<PolicyRule, Long> {
+public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
+
+    Optional<PolicyRule> findByRuleCode(String ruleCode);
 
     List<PolicyRule> findByActiveTrue();
-
-    PolicyRule findByRuleCode(String ruleCode);
 }
--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -2,11 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserAccountRepository
-        extends JpaRepository<UserAccount, Long> {
+import java.util.Optional;
 
-    UserAccount findByEmail(String email);
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+
+    Optional<UserAccount> findByEmail(String email);
 }
