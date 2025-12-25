08e064eaa74af7f994a021f1f7a8934754b4e36a
bala-amypo <bala@amypo.in>
2025-12-25 05:49:00 +0000

    User commit

--- src/main/java/com/example/demo/service/EmployeeProfileService.java
+++ src/main/java/com/example/demo/service/EmployeeProfileService.java
@@ -14,37 +14,3 @@ public interface EmployeeProfileService {
 
     EmployeeProfile updateEmployeeStatus(Long id, boolean active);
 }
-===========
-
-
-
-
-==========
-
-
-
-
-=======
-
-
-
-
-==========
-
-
-
-
-============
-
-package com.example.demo.service;
-
-import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.AuthResponse;
-import com.example.demo.dto.RegisterRequest;
-
-public interface AuthService {
-
-    AuthResponse register(RegisterRequest request);
-
-    AuthResponse login(AuthRequest request);
-}
